23d898394a64655b58ab166e4e9bfca53b67e985
CamiloMartinezCDS <cmartinez@codigodelsur.com>
2018-06-05 16:22:00 -0300

    Final fixes

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
+const { Pool } = require('pg')
+const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+
+const pool = new Pool({
+  connectionString: 'postgres://qhxindpxmbvwzk:a2d9f5349e93980317295d2fa58061533210fb21de3cf7f516180dc1f3427dee@ec2-54-204-2-26.compute-1.amazonaws.com:5432/d75omanv9qj10q',
+  ssl: true
+})
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .get('/times', (req, res) => {
+    let result = ''
+    const times = process.env.TIMES || 5
+    for (i = 0; i < times; i++) {
+      result += i + ' '
+    }
+    res.send(result)
+  })
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      res.render('pages/db', result);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
